504e8380c76ec266c343e20402c81a74d6975c4f
Camille Onoda <info@camilleonoda.com>
2024-01-11 15:55:14 +0900

    Create a capitalizeFirstLetter function to format the result of the game.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const choiceArray = ['rock', 'scissors', 'paper'];
 
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 function getRandomChoice(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
 }
@@ -10,14 +14,17 @@ console.log(playerSelection);
 console.log(computerSelection);
 
 function playRound(player, computer) {
+    player = capitalizeFirstLetter(player);
+    computer = capitalizeFirstLetter(computer);
+
     if (player == computer) {
         console.log("It's a tie!");
     } else if ((player == "rock" && computer == "scissors" ) ||
     (player == "scissors" && computer == "paper" ) ||
     (player == "paper" && computer == "rock" )) {
-        console.log("You win!");
+        console.log(`You win! ${player} beats ${computer}.`);
     } else {
-        console.log("You lose!");
+        console.log(`You lose! ${computer} beats ${player}.`);
     }
 }
 
