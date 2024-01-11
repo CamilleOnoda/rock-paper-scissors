05d48bc4732ffc87a6f69b04cc4e0576fac47f6b
Camille Onoda <info@camilleonoda.com>
2024-01-11 16:44:37 +0900

    Fix bug by moving the capitalizeFirstLetter function in the console.log()

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const choiceArray = ['rock', 'scissors', 'paper'];
+const score = 5;
+let playerScore = 0;
+let computerScore = 0;
 
 function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -8,24 +11,30 @@ function getRandomChoice(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-let playerSelection = getRandomChoice(choiceArray);
-let computerSelection = getRandomChoice(choiceArray);
-console.log(playerSelection);
-console.log(computerSelection);
-
 function playRound(player, computer) {
-    player = capitalizeFirstLetter(player);
-    computer = capitalizeFirstLetter(computer);
-
     if (player == computer) {
         console.log("It's a tie!");
     } else if ((player == "rock" && computer == "scissors" ) ||
     (player == "scissors" && computer == "paper" ) ||
     (player == "paper" && computer == "rock" )) {
-        console.log(`You win! ${player} beats ${computer}.`);
+        console.log(`You win! ${capitalizeFirstLetter(player)} beats ${capitalizeFirstLetter(computer)}.`);
+        return playerScore++;
     } else {
-        console.log(`You lose! ${computer} beats ${player}.`);
+        console.log(`You lose! ${capitalizeFirstLetter(computer)} beats ${capitalizeFirstLetter(player)}.`);
+        return computerScore++;
     }
 }
 
-playRound(playerSelection, computerSelection)
+function game() {
+    while (playerScore < score && computerScore < score) {
+        let playerSelection = getRandomChoice(choiceArray);
+        let computerSelection = getRandomChoice(choiceArray);
+
+        playRound(playerSelection, computerSelection);
+
+        console.log(`Player score: ${playerScore}`);
+        console.log(`Computer score: ${computerScore}`);
+    }
+}
+
+game();
