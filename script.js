2278541342d6bd3fb4604f332e7e5ce11a9ceb10
Camille Onoda <info@camilleonoda.com>
2024-01-22 14:21:22 +0900

    Change the const variable and function name for clarity. Add the logic to handle the case when the user does not enter a valid input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const choiceArray = ['rock', 'scissors', 'paper'];
+const choice = ['rock', 'scissors', 'paper'];
 const score = 5;
 let playerScore = 0;
 let computerScore = 0;
@@ -7,16 +7,18 @@ function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function getRandomChoice(choices) {
+function computerChoice(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(player, computer) {
     if (player == computer) {
         console.log("It's a tie!");
-    } else if ((player == "rock" && computer == "scissors" ) ||
+    } else if (
+    (player == "rock" && computer == "scissors" ) ||
     (player == "scissors" && computer == "paper" ) ||
-    (player == "paper" && computer == "rock" )) {
+    (player == "paper" && computer == "rock" )
+    ) {
         console.log(`You win! ${capitalizeFirstLetter(player)} beats ${capitalizeFirstLetter(computer)}.`);
         return playerScore++;
     } else {
@@ -27,8 +29,11 @@ function playRound(player, computer) {
 
 function game() {
     while (playerScore < score && computerScore < score) {
-        let playerSelection = window.prompt("Choose between rock, paper or scissors:").toLowerCase();
-        let computerSelection = getRandomChoice(choiceArray);
+        let playerSelection = "";
+        while (!choice.includes(playerSelection)) {
+            playerSelection = window.prompt("Choose between rock, paper or scissors:").toLowerCase();
+        }
+        let computerSelection = computerChoice(choice);
 
         playRound(playerSelection, computerSelection);
 
