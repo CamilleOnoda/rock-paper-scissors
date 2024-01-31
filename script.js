6b9df964e22b3fd21c0ffc763ac4197b019ecd35
CamilleOnoda <info@camilleonoda.com>
2024-01-31 12:24:49 +0900

    Add logic to prompt the user if they want to restart the game or not.

--- script.js
+++ script.js
@@ -27,11 +27,28 @@ function playRound(player, computer) {
     }
 }
 
-function game() {
+function restartGame() {
+    let playAgain = "";
+    while (!(playAgain === "yes" && !playAgain === "no")) {
+        playAgain = window.prompt("Do you want to play again? (yes or no)");
+        playAgain = playAgain.toLowerCase();
+        if (playAgain === "yes") {
+            playerScore = 0;
+            computerScore = 0;
+            game();
+        } else if (playAgain === "no") {
+            alert("Thank you for playing!");
+            break;
+        }
+    };
+};
+
+function game() {   
     while (playerScore < score && computerScore < score) {
         let playerSelection = "";
         while (!choice.includes(playerSelection)) {
-            playerSelection = window.prompt("Choose between rock, paper or scissors:").toLowerCase();
+            playerSelection = window.prompt("Choose between rock, paper or scissors:");
+            playerSelection = playerSelection.toLowerCase();
         }
         let computerSelection = computerChoice(choice);
 
@@ -40,9 +57,12 @@ function game() {
         console.log(`Player score: ${playerScore}`);
         console.log(`Computer score: ${computerScore}`);
     }
+
     console.log(`Final score: 
     - Player: ${playerScore}
     - Computer: ${computerScore}`);
-}
+
+    restartGame();
+};
 
 game();
